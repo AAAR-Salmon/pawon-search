a9c1217203ed2dc315fa7596bb747dbfb2997d72
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-10-30 12:31:47 +0900

    Change: 形態素解析エンジンを vibrato に変更してなんとなく作成

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use itertools::Itertools;
+use std::fs::File;
+use vibrato::{tokenizer::worker::Worker, Dictionary, Tokenizer};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let characters = ['ぱ', 'お', 'ん', 'こ', 'う', 'ち'];
+
+    let dict = File::open("system.dic")?;
+    let dict = Dictionary::read(dict)?;
+    let tokenizer = Tokenizer::new(dict);
+    let mut worker = tokenizer.new_worker();
 
     for length in 3..=10 {
         for word in (0..length)
             .map(|_| characters.iter())
             .multi_cartesian_product()
             .map(|v| v.into_iter().collect::<String>())
+            // .filter(|s| !s.starts_with("ん"))
+            .filter(|s| is_word(&mut worker, s))
         {
             println!("{}", word);
         }
     }
+
+    Ok(())
+}
+
+fn is_word(worker: &mut Worker, s: &str) -> bool {
+    worker.reset_sentence(s);
+    worker.tokenize();
+
+    worker.token_iter().count() == 1
 }
