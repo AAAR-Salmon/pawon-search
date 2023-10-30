5b308a9ae1dd3276e81764d14b1e9522c81ea1b8
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-10-30 12:29:42 +0900

    Fix: そもそも全組文字列ミスってた

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ fn main() {
     let characters = ['ぱ', 'お', 'ん', 'こ', 'う', 'ち'];
 
     for length in 3..=10 {
-        for maybe_word in characters
-            .iter()
-            .cycle()
-            .take(characters.len() * length)
-            .permutations(length)
+        for word in (0..length)
+            .map(|_| characters.iter())
+            .multi_cartesian_product()
+            .map(|v| v.into_iter().collect::<String>())
         {
-            let maybe_word = maybe_word.into_iter().collect::<String>();
-            println!("{}", maybe_word);
+            println!("{}", word);
         }
     }
 }
